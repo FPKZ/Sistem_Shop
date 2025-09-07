ce4dc7ff0034d2456b7182e2b1be9650a4826d1e
FPKZ <felipe.gato5433@gmail.com>
2025-09-06 21:33:07 -0300

    polindo

--- src/pages/components/modal/CadastroIntenModal.jsx
+++ src/pages/components/modal/CadastroIntenModal.jsx
@@ -19,6 +19,14 @@ export default function CadastroModal({ visible, onClose, onSubmit }) {
         <form onSubmit={handleSubmit}>
           <input name="nome" placeholder="Nome do item" required />
           <input name="descricao" placeholder="Descrição" required />
+          <input name="cor" placeholder="Cor" required />
+          <input name="imagem" id="imagem" type="file" accept="image/*" />
+          <input name='tamanho' placeholder='Tamanho' />
+          <input name="valor_venda" placeholder="Valor de Venda" type="number" step="0.01" required />
+          <input name="valor_compra" placeholder="Valor de Compra" type="number" step="0.01" required />
+          <input name="estoque_atual" placeholder="Estoque Atual" type="number" required />
+          <input name="codigo_barras" placeholder="Código de Barras" />
+          <input name="categoria_id" placeholder="ID da Categoria" type="number" required />
           <button type="submit">Salvar</button>
         </form>
         <button className="fechar" onClick={onClose}>Fechar</button>
--- src/pages/produtos/components/Produto.jsx
+++ src/pages/produtos/components/Produto.jsx
@@ -22,7 +22,7 @@ function Produto({produtos, deleteProduto}) {
     };
     console.log(produtos)
     return (
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mb-4">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4 mb-4">
             {
                 produtos.map(produto => (
                     <div className="col" key={produto.id}>
