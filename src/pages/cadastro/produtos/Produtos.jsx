b878dfaffadf8aa0fb0e88551a57e57a3f37bba6
FPKZ <felipe.gato5433@gmail.com>
2025-09-09 15:06:02 -0300

    layout cadastrar produtos

--- src/pages/cadastro/produtos/Produtos.jsx
+++ src/pages/cadastro/produtos/Produtos.jsx
@@ -10,56 +10,81 @@ export default function Produtos(){
         console.log(data)
     }
     return (
-        <div className="">
-            <form onSubmit={handleSubimit}>
-                <div>
-                    <label for="nomeProduto" className="form-label">Nome Do Produto</label>
-                    <input className="form-control" name="nome" id="nomeProduto" type="text" placeholder="nome" required/>
-                    <label for="imgProduto" className="form-label">Imagem do Produto</label>
-                    <input className="form-control" name="img" id="imgProduto" type="file" />
-                    <label for="corProduto" className="form-label">Cor do Produto</label>
-                    <input className="form-control form-control-color" name="cor" id="corProduto" type="color" placeholder="Cor" value=" "/>
-                    <label for="tamanhoProduto" className="form-label">Tamanho do Produto</label>
-                    <input className="form-control" name="tamanho" id="tamanhoProduto" type="text" placeholder="Tamanho" />
-                    <label for="categoriaProduto" className="form-label">Categoria do Produto</label>
-                    <div className="btn-group">
-                        <button type="button" className="dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false">{categoria}</button>
-                        <input className="form-control" type="hidden" name="categoria" value={categoria} />
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#" onClick={() => setCategoria("cat")}>cat</a></li>
-                            <li><a className="dropdown-item" href="#">dasd</a></li>
-                            <li><hr class="dropdown-divider"></hr></li>
-                            <li><a className="dropdown-item" href="#">dasd</a></li>
-                        </ul>
+        <div className="row row-cols-2 w-100 p-3 d-flex gap-4">
+            <form onSubmit={handleSubimit} className="row-cols-1 w-100">
+                <div className="row">
+                    <div className="col-md-12">
+                        <label htmlFor="nomeProduto" className="form-label">Nome Do Produto</label>
+                        <input className="form-control" name="nome" id="nomeProduto" type="text" placeholder="nome" required/>
+                    </div>
+                    <div className="col-md-4">
+                        <label htmlFor="imgProduto" className="form-label">Imagem do Produto</label>
+                        <input className="form-control" name="img" id="imgProduto" type="file" />
+                    </div>
+                    <div className="col-md-2">
+                        <label htmlFor="corProduto" className="form-label">Cor do Produto</label>
+                        <input className="form-control form-control-color" name="cor" id="corProduto" type="color" placeholder="Cor" />
+                    </div>
+                    <div className="col-md-2">
+                        <label htmlFor="tamanhoProduto" className="form-label">Tamanho do Produto</label>
+                        <input className="form-control" name="tamanho" id="tamanhoProduto" type="text" placeholder="Tamanho" />
+                    </div>
+                    <div className="col-md-4 d-flex flex-column">
+                        <label htmlFor="categoriaProduto" className="form-label">Categoria do Produto</label>
+                        <div className="btn-group">
+                            <button type="button" className="dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false">{categoria}</button>
+                            <input className="form-control" id="categoriaProduto" type="hidden" name="categoria" value={categoria} />
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#" onClick={() => setCategoria("cat")}>cat</a></li>
+                                <li><a className="dropdown-item" href="#">dasd</a></li>
+                                <li><hr className="dropdown-divider"></hr></li>
+                                <li><a className="dropdown-item" href="#">dasd</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <label for="notaProduto" className="form-label">Nota do Produto</label>
-                    <div className="btn-group">
-                        <button type="button" className="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">{nota}</button>
-                        <input className="form-control" type="hidden" name="categoria" value={nota} required/>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#" onClick={() => setCategoria("cat")}>cat</a></li>
-                            <li><a className="dropdown-item" href="#">dasd</a></li>
-                            <li><hr class="dropdown-divider"></hr></li>
-                            <li><a className="dropdown-item" href="#">dasd</a></li>
-                        </ul>
+                <div className="row">
+                    <div className="col-md-2 d-flex flex-column">
+                        <label htmlFor="notaProduto" className="form-label">Nota do Produto</label>
+                        <div className="btn-group">
+                            <button type="button" className="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">{nota}</button>
+                            <input className="form-control" id="notaProduto" type="hidden" name="categoria" value={nota} required/>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#" onClick={() => setCategoria("cat")}>cat</a></li>
+                                <li><a className="dropdown-item" href="#">dasd</a></li>
+                                <li><hr className="dropdown-divider"></hr></li>
+                                <li><a className="dropdown-item" href="#">dasd</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <label for="valorCompraProduto" className="form-label">Valor de Compra</label>
-                    <div class="input-group">
-                        <span class="input-group-text">$</span>
-                        <input className="form-control" name="valor_compra" id="valorCompraProduto" type="text" placeholder="valor de compra" required/>
-                        <span class="input-group">0.00</span>
+                    <div className="col-md-2">
+                        <label htmlFor="valorCompraProduto" className="form-label">Valor de Compra</label>
+                        <div className="input-group">
+                            <span className="input-group-text">R$</span>
+                            <input className="form-control" name="valor_compra" id="valorCompraProduto" type="text" placeholder="0.00" required/>
+                        </div>
                     </div>
-                    <label for="valorVendaProduto" className="form-label">Valor de Venda</label>
-                    <input className="form-control" name="valor_venda" id="valorVendaProduto" type="text" placeholder="valor de venda" required/>
-                    <label for="LucroProduto" className="form-label">Lucro</label>
-                    <input className="form-control" name="lucro" id="LucroProduto" type="number" placeholder="lucro" required/>
-                    <label for="entradaEstoqueProduto" className="form-label">Entrada do Produto</label>
-                    <input className="form-control" name="entrada_estoque" id="entradaEstoqueProduto" type="text" placeholder="quantidade de entrada" required/>
+                    <div className="col-md-2">
+                        <label htmlFor="valorVendaProduto" className="form-label">Valor de Venda</label>
+                        <div className="input-group">
+                            <span className="input-group-text">R$</span>
+                            <input className="form-control" name="valor_venda" id="valorVendaProduto" type="text" placeholder="valor de venda" required/>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <label htmlFor="LucroProduto" className="form-label">Lucro</label>
+                        <div className="input-group">
+                            <span className="input-group-text">R$</span>
+                            <input className="form-control" name="lucro" id="LucroProduto" type="number" placeholder="lucro" required/>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <label htmlFor="entradaEstoqueProduto" className="form-label">Entrada do Produto</label>
+                        <input className="form-control" name="entrada_estoque" id="entradaEstoqueProduto" type="text" placeholder="quantidade de entrada" required/>
+                    </div>
                 </div>
-                <div>
-                    <label for="descricaoProduto" className="form-label">Descrição do Produto</label>
+                <div className="col-md-12 mb-3">
+                    <label htmlFor="descricaoProduto" className="form-label">Descrição do Produto</label>
                     <input className="form-control" name="descricao" type="text" id="descricaoProduto" placeholder="Descrição"/>
                 </div>
                 <button type="submit">Salvar</button>
