c663158447939ea45dcf142c7f87ee35bfbb1463
fpkz <felipe.gato5433@gmail.com>
2025-10-15 19:52:15 -0300

    att sequelize

--- backend/database/sequelize.js
+++ backend/database/sequelize.js
@@ -12,16 +12,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
     },
 })
 
