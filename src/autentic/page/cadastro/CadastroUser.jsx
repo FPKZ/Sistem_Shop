e37edfc7933410f6f8dd2bdd9f7b4ac84f87dff0
fpkz <felipe.gato5433@gmail.com>
2025-10-14 21:48:14 -0300

    att

--- src/autentic/page/cadastro/CadastroUser.jsx
+++ src/autentic/page/cadastro/CadastroUser.jsx
@@ -1,10 +1,13 @@
-
+import { Container, Row, Col } from "react-bootstrap"
 
 export default function CadastroUser() {
     return (
-        <div>
-            <h2>Cadastro de Usuário</h2>
-            <p>Formulário de cadastro de usuário em construção.</p>
-        </div>
+        <Container>
+            <div>
+                <h2>Cadastro de Usuário</h2>
+                <p>Formulário de cadastro de usuário em construção.</p>
+            </div>
+
+        </Container>
     );
 }
