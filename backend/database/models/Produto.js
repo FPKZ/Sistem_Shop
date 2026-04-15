c5503ebfeae492db7aefd83222bba49af60e1a3e
fpkz <felipe.gato5433@gmail.com>
2026-04-15 10:31:59 -0300

    adicionando hook de alteração de nome

--- backend/database/models/Produto.js
+++ backend/database/models/Produto.js
@@ -10,4 +10,21 @@ const Produto = sequelize.define('Produto', {
     tableName: 'Produtos'
 });
 
+// Hook para sincronizar o nome com os Itens de Estoque após a atualização
+Produto.addHook('afterUpdate', async (produto, options) => {
+    // Só dispara se o nome tiver sido alterado
+    if (produto.changed('nome')) {
+        const ItemEstoque = produto.sequelize.models.ItemEstoque;
+        if (ItemEstoque) {
+            await ItemEstoque.update(
+                { nome: produto.nome },
+                { 
+                    where: { produto_id: produto.id },
+                    transaction: options.transaction // Mantém a transação se houver uma
+                }
+            );
+        }
+    }
+});
+
 export default Produto;
